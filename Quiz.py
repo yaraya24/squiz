24131322db39687a4f5eee03001a1d9202759f89
Garret Blankenship <garret.blankenship@coderacademy.edu.au>
2020-07-08 08:55:14 +1000

    modified get_questions in Quiz class to also decode the question content

--- Quiz.py
+++ Quiz.py
@@ -16,9 +16,18 @@ class Quiz():
             api_url = "https://opentdb.com/api.php"
             response = requests.get(f"{api_url}?type=multiple&amount={amount}&category={category}")
             content = json.loads(response.text)
+            cls.decode_questions(content["results"])
             return content["results"]
         except Exception:
             return []
+    
+    @classmethod
+    def decode_questions(cls, questions: list) -> None:
+        for question in questions:
+            question["question"] = unescape(question["question"])
+            question["correct_answer"] = unescape(question["correct_answer"])
+            for option in question["incorrect_answers"]:
+                option = unescape(option)
     
     def __init__(self) -> None:
         self.questions: list = []
@@ -28,10 +37,10 @@ class Quiz():
         for raw_question in raw_questions:
             options = []
             for option in raw_question["incorrect_answers"]:
-                options.append(unescape(option))
+                options.append(option)
             
-            title = unescape(raw_question["question"])
-            answer = unescape(raw_question["correct_answer"])
+            title = raw_question["question"]
+            answer = raw_question["correct_answer"]
             options.append(answer)
 
             question = Question(title, options, answer)
